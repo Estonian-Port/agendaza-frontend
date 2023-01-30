96f653a907761f7cf4ecca536fa2bff35b3bf92a
RdzSebastian <RdzSebastian@gmail.com>
2023-01-30 15:45:33 -0300

    Refactor visual de agenda y seleccionar agenda

---
+++ src/app/layout/nuevo-evento/nuevo-evento.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-nuevo-evento',
+  templateUrl: './nuevo-evento.component.html',
+  styleUrls: ['./nuevo-evento.component.css']
+})
+export class NuevoEventoComponent implements OnInit {
+  
+
+  ngOnInit(): void {
+  }
+
+}
