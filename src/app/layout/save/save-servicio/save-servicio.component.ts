73919e8f6700bdb00a75fa0ca256092c6496e84a
rdzsebastian <rdzsebastian@gmail.com>
2025-07-10 00:21:36 -0300

    Nueva funcionalidad de reutilizacion en servicios

--- src/app/layout/save/save-servicio/save-servicio.component.ts
+++ src/app/layout/save/save-servicio/save-servicio.component.ts
@@ -1,11 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import * as _ from 'lodash';
 import { fadeOut } from 'src/app/animations/fade-out';
 import { GenericItem, GenericItemEmpresaTipoEvento } from 'src/app/model/GenericItem';
-import { TipoEvento } from 'src/app/model/TipoEvento';
 import { ServicioService } from 'src/app/services/servicio.service';
-import { TipoEventoService } from 'src/app/services/tipo-evento.service';
 
 @Component({
   selector: 'app-save-servicio',
@@ -15,34 +13,32 @@ import { TipoEventoService } from 'src/app/services/tipo-evento.service';
 export class SaveServicioComponent implements OnInit {
   
   genericItem : GenericItemEmpresaTipoEvento = new GenericItemEmpresaTipoEvento(0, "", 0, [])
-  
-  listaTipoEvento : Array<TipoEvento> = []
-
-  listaTipoEventoCorto : Array<TipoEvento> = []
-  listaTipoEventoMedio : Array<TipoEvento> = []
-  listaTipoEventoLargo : Array<TipoEvento> = []
-
-  seleccionado = 1
-  otro = false
-
   listaServicio : Array<GenericItem> = []
 
-  constructor(private servicioService : ServicioService, private tipoEventoService : TipoEventoService, private router : Router) { }
+  otro = false
+  edicion = false
+
+  constructor(private servicioService : ServicioService, private router : Router) { }
   
   async ngOnInit(): Promise<void> {
 
     if(this.servicioService.servicioId){
       this.genericItem = await this.servicioService.getServicio(this.servicioService.servicioId)
       this.servicioService.servicioId = 0
+      this.otro = true
+      this.edicion = true
     }else{
-      this.listaServicio = await this.servicioService.getAllServicio()
+      this.listaServicio = await this.servicioService.getAllServicioAgregar()
+      if(this.listaServicio.length == 0){
+        this.genericItem.id = 0
+        this.onServicioChange()
+      }else{
+        const servicioEdicion = [...this.listaServicio].sort((a, b) => a.nombre.localeCompare(b.nombre))[0];
+
+        this.genericItem.id = servicioEdicion.id
+        this.genericItem.nombre = servicioEdicion.nombre
+      }
     }
-
-    this.listaTipoEvento = await this.tipoEventoService.getAllTipoEventoByEmpresaId()
-
-    this.listaTipoEventoCorto = this.listaTipoEvento.filter(evento => evento.duracion === 'CORTO');
-    this.listaTipoEventoMedio = this.listaTipoEvento.filter(evento => evento.duracion === 'MEDIO');
-    this.listaTipoEventoLargo = this.listaTipoEvento.filter(evento => evento.duracion === 'LARGO');
   }
 
   async save(){
@@ -54,45 +50,14 @@ export class SaveServicioComponent implements OnInit {
     this.router.navigateByUrl('/abmServicio')
   }
 
-  onCheckboxChange(event: any){
-    if (event.target.checked) {
-      this.genericItem.listaTipoEventoId.push(Number(event.target.value))
-    } else {
-      _.pull(this.genericItem.listaTipoEventoId, Number(event.target.value))
-    }
-  }
-
-  isChecked(id : number){
-    if(this.genericItem.listaTipoEventoId != undefined){
-      return this.genericItem.listaTipoEventoId.find(it => it == id)
-    }
-    return false
-  }
-  
-  selectAll(listaTipoEvento : Array<TipoEvento>) {
-    if(!this.areAllSelected(listaTipoEvento)){
-      this.genericItem.listaTipoEventoId = [...new Set([... this.genericItem.listaTipoEventoId, ...listaTipoEvento.map(it => it.id)])]
-    }else{
-      this.genericItem.listaTipoEventoId = this.genericItem.listaTipoEventoId.filter(item => !listaTipoEvento.some(it => it.id == item));
-    }
-  }
-
-  areAllSelected(listaTipoEvento : Array<TipoEvento>) {
-    return listaTipoEvento.every(item => this.genericItem.listaTipoEventoId.includes(item.id));
-
-  }
-
   onServicioChange(): void {
-    console.log("asdasdasd")
-    console.log(this.seleccionado)
-    if (this.seleccionado == 0) {
-      console.log("si")
-
+    if (this.genericItem.id == 0) {
+        this.genericItem = new GenericItemEmpresaTipoEvento(0, "", 0, [])
         this.otro = true;
     } else {
         this.otro = false;
     }
-}
+  }
   
 }
 
