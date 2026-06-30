da03f91bbb222052e91b2e7163d3a0dcb9b57a73
rdzsebastian <rdzsebastian@gmail.com>
2026-06-30 12:41:47 +0900

    Migracion a deploy en branch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-  production: false,
-  REST_SERVER_URL: 'http://localhost:8080'
-  // Puedes dejar aquí tus otras opciones comentadas si quieres
+declare const process: {
+  env: {
+    NG_APP_PRODUCTION: string;
+    NG_APP_REST_SERVER_URL: string;
+  };
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+export const environment = {
+  production: process.env.NG_APP_PRODUCTION === 'true',
+  REST_SERVER_URL: process.env.NG_APP_REST_SERVER_URL
+};
